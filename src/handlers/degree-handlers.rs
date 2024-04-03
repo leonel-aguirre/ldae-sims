47a426b360e5192591b27f6f129e58051ef199d9
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-04-03 03:52:31 -0600

    LDSS-2 - Create degree handler added.

--- src/handlers/degree-handlers.rs
+++ src/handlers/degree-handlers.rs
@@ -6,8 +6,13 @@ use axum::{
     response::IntoResponse,
     Json,
 };
+use serde_json::json;
 
-use crate::{degree_model::DegreeModel, degree_schemas::ParamOptions, shared::AppState};
+use crate::{
+    degree_model::DegreeModel,
+    degree_schemas::{CreateDegreeSchema, ParamOptions},
+    shared::AppState,
+};
 
 // GET
 pub async fn select_all_degrees_handler(
@@ -61,5 +66,45 @@ pub async fn select_degree_by_id_handler(
         "status": "success",
         "degree": degree[0]
     });
+
     Ok(Json(json_response))
+}
+
+// POST
+pub async fn create_degree_handler(
+    State(data): State<Arc<AppState>>,
+    Json(body): Json<CreateDegreeSchema>,
+) -> Result<impl IntoResponse, (StatusCode, Json<serde_json::Value>)> {
+    let query_result = sqlx::query_as!(
+        DegreeModel,
+        "INSERT INTO degrees (name) VALUES ($1) RETURNING *",
+        body.name.to_string(),
+    )
+    .fetch_one(&data.db)
+    .await;
+
+    match query_result {
+        Ok(degree) => {
+            let degree_response = json!({"status": "success","data": json!({
+                "degree": degree
+            })});
+
+            return Ok((StatusCode::CREATED, Json(degree_response)));
+        }
+        Err(e) => {
+            if e.to_string()
+                .contains("duplicate key value violates unique constraint")
+            {
+                let error_response = serde_json::json!({
+                    "status": "fail",
+                    "message": "Degree already exists.",
+                });
+                return Err((StatusCode::CONFLICT, Json(error_response)));
+            }
+            return Err((
+                StatusCode::INTERNAL_SERVER_ERROR,
+                Json(json!({"status": "error","message": format!("{:?}", e)})),
+            ));
+        }
+    }
 }
--- src/routes/degree-routes.rs
+++ src/routes/degree-routes.rs
@@ -1,12 +1,18 @@
 use crate::{
-    degree_handlers::{select_all_degrees_handler, select_degree_by_id_handler},
+    degree_handlers::{
+        create_degree_handler, select_all_degrees_handler, select_degree_by_id_handler,
+    },
     shared::AppState,
 };
-use axum::{routing::get, Router};
+use axum::{
+    routing::{get, post},
+    Router,
+};
 use std::sync::Arc;
 
 pub fn degree_routes() -> Router<Arc<AppState>> {
     Router::new()
         .route("/degrees", get(select_all_degrees_handler))
         .route("/degree/:id", get(select_degree_by_id_handler))
+        .route("/degree", post(create_degree_handler))
 }
