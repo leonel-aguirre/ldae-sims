ae8ce89560318737a0e355658ec4eb469268bbfa
Leonel Aguirre <leonel.aguirre182@gmail.com>
2024-04-03 21:02:45 -0600

    LDSS-3 - Courses table foreign key added.

---
+++ migrations/20240404025905_ldss-3-courses-table-specialization-code-fk-addition.down.sql
@@ -0,0 +1,5 @@
+ALTER TABLE courses
+DROP CONSTRAINT IF EXISTS courses_specialization_code_fkey;
+
+ALTER TABLE courses
+DROP COLUMN IF EXISTS specialization_code;
---
+++ migrations/20240404025905_ldss-3-courses-table-specialization-code-fk-addition.up.sql
@@ -0,0 +1,6 @@
+ALTER TABLE courses
+ADD COLUMN specialization_code VARCHAR(20);
+
+ALTER TABLE courses
+ADD CONSTRAINT courses_specialization_code_fkey
+FOREIGN KEY (specialization_code) REFERENCES specializations(specialization_code);
